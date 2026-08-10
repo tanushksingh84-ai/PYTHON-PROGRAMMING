e57a9383d6c6aa6a2a1135b5472bca3b05b86f82
TANUSHK <tanushksingh84@gmail.com>
2026-08-10 10:55:53 +0530

    More String Porgrams

---
+++ string operations 2.py
@@ -0,0 +1,11 @@
+text="Python123"
+print(text.isdigit())
+print(text.isalpha())
+print(text.isalnum())
+print(text.startswith("Py"))
+print(text.endswith("123"))
+
+name="Tanushk"
+age=18
+#f-string using
+print(f"My name is {name} and I am {"age"} years old.")
---
+++ string using loops.py
@@ -0,0 +1,13 @@
+text="PYTHON"
+for ch in text: #linr by line each word
+    print(ch)
+
+for ch in text: #print the word in one line
+    print(ch,end="")
+
+
+    
+
+for i in range(len(text)):
+    print(text[i])
+
