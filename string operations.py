ea3d6220d3d6836f1e865288dd6655a9d1c5a819
TANUSHK <tanushksingh84@gmail.com>
2026-08-10 10:31:16 +0530

    STRING operations

---
+++ string operations.py
@@ -0,0 +1,11 @@
+text="python programming"
+print(text.lower())
+print(text.upper())
+print(text.title())
+print(text.capitalize())
+print(text.replace("pyhton","Java"))
+print(text.split())
+print(text.find("pro"))
+print(text.count("m"))
+print(text.strip())
+
